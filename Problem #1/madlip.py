69fbd9bca8f8e60e4b3ce1dd582fe7f021dfe647
Will Morris <wdm9727@utulsa.edu>
2024-03-01 15:41:18 -0600

    formatted problem 1

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -1,15 +1,8 @@
+"""Will Morris, Friday Afternoon Lab"""
+
 def food_input():
     user_input = input()
     tokens = user_input.split()
-    
-    # line = "Eating num things a day makes you happy and healthy"
-    # Newline = line.replace ("num", tokens[1])
-    # Newline = line.replace ("things", tokens[0])
-
-    # print(Newline)
-    # print(tokens)
-
-    #type you while  loop here 
 
     madLib = ["Eating","num","things","a day keeps you happy and healthy."]
     madLib[1] = tokens[1]
@@ -19,9 +12,6 @@ def food_input():
 
     print(newLine)
 
-
-
-    
 
 if __name__ == "__main__":
     food_input()
