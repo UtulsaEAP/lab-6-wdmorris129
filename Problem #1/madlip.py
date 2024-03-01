8b6b1d8a96a1685ad7d3df6ae7912974db0839a8
Will Morris <wdm9727@utulsa.edu>
2024-03-01 15:24:38 -0600

    Added code for problem 1

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -1,7 +1,25 @@
 def food_input():
     user_input = input()
     tokens = user_input.split()
-    #type you while  loop here
+    
+    # line = "Eating num things a day makes you happy and healthy"
+    # Newline = line.replace ("num", tokens[1])
+    # Newline = line.replace ("things", tokens[0])
+
+    # print(Newline)
+    # print(tokens)
+
+    #type you while  loop here 
+
+    madLib = ["Eating","num","things","a day keeps you happy and healthy."]
+    madLib[1] = tokens[1]
+    madLib[2] = tokens[0]
+
+    newLine = ' '.join([str(elem) for elem in madLib])
+
+    print(newLine)
+
+
 
     
 
