8fb9b3583d7cfbbaa86d06fbd8974fdddc6dcce4
Will Morris <wdm9727@utulsa.edu>
2024-03-01 15:39:59 -0600

    Added code for problem 2

--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -1,5 +1,28 @@
 def check_palindrome(user_input):
- #type your code here
+    new_input = user_input[::-1]
+    
+    if new_input == user_input:
+        print("palindrome: {a}".format(a = user_input))
+    else:
+        print("not a palindrome: {b}".format(b = user_input))
+
+
+
+
+
 if __name__ == "__main__":
     user_input = input()
     check_palindrome(user_input)
+
+
+# def reverse(string):
+#     string = string[::-1]
+#     return string
+ 
+# s = "Geeksforgeeks"
+ 
+# print("The original string is : ", end="")
+# print(s)
+ 
+# print("The reversed string(using extended slice syntax) is : ", end="")
+# print(reverse(s))
