5a2b0896cb74f83a0ffeaface06d7fa029f0e288
Will Morris <wdm9727@utulsa.edu>
2024-03-08 11:38:00 -0600

    updated problem 5 code for formatting

--- Problem#5/elementsrange.py
+++ Problem#5/elementsrange.py
@@ -4,14 +4,16 @@ def filter_and_print_range(input_list, min_val, max_val):
 
 if __name__ == '__main__':
     # Get input for the list of integers
-    user_input = input("Enter a space-separated string of numbers: ")
-    integer_list = [int(num) for num in user_input.split()]
+    input_list = input("Enter a space-separated string of numbers: ")
+    integer_list = [int(num) for num in input_list.split()]
 
     # Get input for the range (min and max values)
     input_range = input("Enter the min and max values separated by a space: ")
-    min_val, max_val = user_input
     min_val, max_val = map(int, input_range.split())
 
     # Call the function to filter and print the numbers in the given range
     filter_and_print_range(integer_list, min_val, max_val)
-    
+
+
+
+
