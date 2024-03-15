66c54bba534088052a6babcdbc6e95f15c0df030
Will Morris <wdm9727@utulsa.edu>
2024-03-14 19:45:55 -0500

    edited problem 6 code

--- Problem#6/contactlist.py
+++ Problem#6/contactlist.py
@@ -1,20 +1,19 @@
 """Will Morris, Friday Afternoon"""
 def process_user_contacts(user_input):
-
-    
+ 
     # Get contact name from input, output contact's phone number
-    contact_name = input("Enter the contact name: ")
     contacts = dict(pair.split(',') or pair.split(" ") for pair in user_input.split())
+    contact_name = input("Enter the contact name: ")
     if contact_name in contacts:
         print(contacts[contact_name])
     else:
         print("Contact not found.")
 
-    
-   
 if __name__ == '__main__':
     # Get input for word pairs
     user_input = input("Enter word pairs (name, phone number): ")
 
     # Call the function to process user contacts
     process_user_contacts(user_input)
+
+
